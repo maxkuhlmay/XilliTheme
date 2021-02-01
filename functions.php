87bea3997277cacb6b4f28ebb1a0b23a2c1e85ae
Max Kuhlmay <max.kuhlmay@icloud.com>
2021-02-01 11:59:04 +0000

    Added Bootstrap CSS and JS

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@
     }
 
     add_action("wp_enqueue_scripts", "load_css");
+
+    function load_js() {
+        wp_register_script("bootstrap", get_template_directory_uri() . "/js/bootstrap.min.js", "jquery", false, true);
+        wp_enqueue_script("bootstrap");
+    }
+
+    add_action("wp_enqueue_scripts", "load_js");
